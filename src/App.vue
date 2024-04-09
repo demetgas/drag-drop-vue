119904f8fb527be9f8a6c95fb7d9f7f30666ce54
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-09 10:39:43 +0200

    renamed

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcomeeeeeee to Your Vue.js App" />
+  <div class="App">
+    <TaskCard />
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import TaskCard from "./components/TaskCard.vue";
 
 export default {
-  name: "App",
   components: {
-    HelloWorld,
+    TaskCard,
   },
 };
 </script>
 
 <style>
+#root {
+  background-color: #292727;
+}
+.App {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #292727;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/TaskCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">Hello</div>
+</template>
+
+<script>
+export default {
+  name: "TaskCard",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
