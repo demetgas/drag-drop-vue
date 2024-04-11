ba6f304a2a29d8b92636f67b2ea493f653b25c29
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-11 14:57:36 +0200

    added data to another file

---
+++ src/data.js
@@ -0,0 +1,31 @@
+export const data = [
+  {
+    id: "To Do",
+    tasks: [
+      { name: "task 1" },
+      { name: "task 2" },
+      { name: "task 3" },
+      { name: "task 4" },
+      { name: "task 5" },
+      { name: "task 6" },
+      { name: "task 7" },
+      { name: "task 8" },
+    ],
+    backgroundColor: "#A54B4A",
+  },
+  {
+    id: "In Progress",
+    tasks: [{ name: "task 9" }, { name: "task 10" }],
+    backgroundColor: "#4A71A5",
+  },
+  {
+    id: "Done",
+    tasks: [{ name: "task 11" }, { name: "task 12" }, { name: "task 13" }],
+    backgroundColor: "#4AA561",
+  },
+  {
+    id: "Even",
+    tasks: [{ name: "task 14" }, { name: "task 16" }, { name: "task 18" }],
+    backgroundColor: "#A5A14A",
+  },
+];
